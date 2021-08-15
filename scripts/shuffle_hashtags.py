29182c897276ed9664a127008028b2d06a1d8d47
praacheengyaan <88571894+praacheengyaan@users.noreply.github.com>
2021-08-15 12:13:06 +0530

    Update shuffle_hashtags.py

--- scripts/shuffle_hashtags.py
+++ scripts/shuffle_hashtags.py
@@ -4,19 +4,19 @@ tags = [
   # primary hashtags
   'ancientknowledge praacheengyaan sanatandharma bhagavadgita',
   # secondary hashtags
-  'ancient knowledge education spirituality hinduism krishna lordkrishna harekrishna sanatandharma sanatani mahabharat ramayana vedas hindu wisdom india meditation yoga namaste bhagavadgitaquotes',
+  'ancient knowledge education spiritual spirituality hinduism krishna lordkrishna harekrishna sanatandharma sanatani mahabharat ramayana vedas hindu wisdom india meditation yoga namaste bhagavadgitaquotes science scripture universe god creation galaxy cosmos learn learning success health healthymind mind believe faith hope peace grace pray soul',
   # optional hashtags
-  'instagram insta instalike instadaily instagood lifestyle lifelessons motivationalquotes motivation life book likeaboss likefollow quotes love bookstagram'
+  'instagram insta instalike instadaily instagood lifestyle lifelessons motivationalquotes motivation life book likeaboss likefollow quotes love bookstagram bestoftheday happy kind joy goodvibes'
 ]
 
 def shuffle_hashtags(tags):
-    total_hashtag_limit = random.randrange(18,26)
+    total_hashtag_limit = random.randrange(22,28)
 
     primary_hashtag_list = ['#' + _ for _ in tags[0].split()]
     primary_hashtag_limit = len(primary_hashtag_list)
 
     secondary_hashtag_list = ['#' + _ for _ in tags[1].split()]
-    secondary_hashtag_limit = int((total_hashtag_limit - primary_hashtag_limit) * 0.60)
+    secondary_hashtag_limit = int((total_hashtag_limit - primary_hashtag_limit) * 0.50)
 
     optional_hashtag_list = ['#' + _ for _ in tags[2].split()]
     optional_hashtag_limit = total_hashtag_limit - (primary_hashtag_limit + secondary_hashtag_limit)
